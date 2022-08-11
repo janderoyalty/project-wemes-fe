7dc4ade01764f023be90f355ecf2e912bc52f332
Jande Royalty <janderoyalty@gmail.com>
2022-08-11 09:37:46 -0500

    removes serviceworker from index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-serviceWorker.register();
+// serviceWorker.register();
 root.render(
   <React.StrictMode>
     <App />
