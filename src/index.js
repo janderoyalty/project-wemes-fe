d6f4315ecdfc2bef5597dbc53e97822c9ab8e723
Jande Royalty <janderoyalty@gmail.com>
2022-08-11 09:30:21 -0500

    removes Procfile

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+serviceWorker.register();
 root.render(
   <React.StrictMode>
     <App />
